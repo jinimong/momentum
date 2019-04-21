53342f0071ffddd67be29c313a90eb606b307c34
jwkim <phoongdeng@gmail.com>
2019-04-21 18:34:14 +0900

    Add delete button function
    삭제버튼 기능 동작 구현

--- todo.js
+++ todo.js
@@ -3,7 +3,18 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LOCAL_STORAGE = "toDos";
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    })
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LOCAL_STORAGE, JSON.stringify(toDos));
@@ -15,6 +26,7 @@ function paintToDo(text) {
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.value = "✅";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
