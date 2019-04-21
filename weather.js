c267e005017473f5b67442fc2d18b31c9a723179
jwkim <phoongdeng@gmail.com>
2019-04-21 23:24:09 +0900

    Add weather info used API
    openweahermap API 이용하여 온도/위치 정보 출력

--- weather.js
+++ weather.js
@@ -1,16 +1,34 @@
+const weather = document.querySelector('.js-weather');
+
 const COORDS = 'coords';
 const API_KEY = '241051bf13976dd3ddf8b8d9f247255e';
+
+function getWeather(lat, lng) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${place} @ ${temperature}°C`;
+    })
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+
     const coordsObj = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
+        lat,
+        lng
     }
     saveCoords(coordsObj);
+    getWeather(lat, lng);
 }
 
 function handleGeoError() {
@@ -22,11 +40,12 @@ function askForCoords() {
 }
 
 function loadCoors() {
-    const loadedCoors = localStorage.getItem(COORDS);
-    if (loadedCoors === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
         askForCoords();
     } else {
-        // getCoors();
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.lat, parseCoords.lng);
     }
 }
 
